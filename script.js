8bf4dea6920ab7c1ca87da8fb557c0b030cb10e1
AndreeaForce <andreeaforce@gmail.com>
2017-06-09 01:08:35 +0300

    versiunea 1.0

---
+++ script.js
@@ -0,0 +1,51 @@
+function openFunction1() {
+    var open = document.getElementById('close-box1');
+    var icon = document.getElementById('fa-open');
+    icon.classList.toggle("fa-minus");
+    
+    if (open.style.display == 'block') {
+        (open.style.display = 'none');
+    } else {
+        open.style.display = 'block';
+    }
+}
+function openFunction2() {
+    var open = document.getElementById('close-box2');
+    var icon = document.getElementById('fa-open2');
+    icon.classList.toggle("fa-minus");
+    
+    if (open.style.display == 'block') {
+        (open.style.display = 'none');
+    } else {
+        open.style.display = 'block';
+    }
+}
+function openFunction3() {
+    var open = document.getElementById('close-box3');
+    var icon = document.getElementById('fa-open3');
+    icon.classList.toggle("fa-minus");
+    
+    if (open.style.display == 'block') {
+        (open.style.display = 'none');
+    } else {
+        open.style.display = 'block';
+    }
+}
+function showTxt() {
+    var x = document.getElementById('hidden-txt');
+    var y = document.getElementById('show-txt');
+    var z = document.getElementById('hide-txt');
+    
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "block";
+}
+function hideTxt() {
+    var x = document.getElementById('hidden-txt');
+    var y = document.getElementById('show-txt');
+    var z = document.getElementById('hide-txt');
+    
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+}
